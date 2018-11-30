81440fcc4313f47d396e3a3ceff610d0d7a52fd0
bwall72 <brandonw@live.com>
2018-11-29 20:07:08 -0500

    able to read database and update seekarc's

--- app/src/main/java/com/example/brand/seniordesigndemo/MonitorActivity.kt
+++ app/src/main/java/com/example/brand/seniordesigndemo/MonitorActivity.kt
@@ -2,7 +2,10 @@ package com.example.brand.seniordesigndemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.*
 import com.triggertrap.seekarc.SeekArc
 
 class MonitorActivity : AppCompatActivity() {
@@ -12,12 +15,46 @@ class MonitorActivity : AppCompatActivity() {
     private lateinit var rpmSeekArc: SeekArc
     private lateinit var rpmText: TextView
 
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var firebaseDatabaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monitor)
 
         initLayout()
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        val deviceId = intent.getStringExtra("DEVICE_ID")
+        Log.d("DEVICE ID", deviceId)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseDatabaseReference = firebaseDatabase.getReference("devices").child(deviceId)
+
+        firebaseDatabaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("snapshot", p0.toString())
+                Toast.makeText(this@MonitorActivity,
+                        "Failed to retrieve reading",
+                        Toast.LENGTH_SHORT)
+                        .show()
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val reading = dataSnapshot.value.toString().split(" ")
+                Log.d("HERE", dataSnapshot.toString())
+                if (reading.size == 2) {
+                    speedText.text = reading[0]
+                    speedSeekArc.progress = reading[0].toInt()
+
+                    rpmText.text = reading[1]
+                    rpmSeekArc.progress = reading[1].toInt()
+                }
+            }
+
+        })
     }
 
     private fun initLayout() {
